a526f684468495e241ef50095562a904add5138e
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-04-29 02:07:27 +0300

    seed: Remove unneeded libqct_resampler
    Change-Id: I2ca1b8af9bfb4c73fae1cab56e90957e3c0477a4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -104,7 +104,6 @@ PRODUCT_PACKAGES += \\
     qcrilhook
 
 PRODUCT_PACKAGES += \\
-    libqct_resampler \\
     libmm-abl \\
     libmm-disp-apis
 
@@ -227,17 +226,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_PATH := \$(TARGET_OUT_SHARED_LIBRARIES)
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := libqct_resampler
-LOCAL_MODULE_OWNER := google
-LOCAL_SRC_FILES := proprietary/vendor/lib/libqct_resampler.so
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
-LOCAL_PROPRIETARY_MODULE := true
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
