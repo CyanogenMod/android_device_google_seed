dfd8b6e6d2331c03255ff5c3db3c5feefd1c9249
Arvin Quilao <arquilao@gmail.com>
2016-05-30 11:34:13 +0800

    Enable OTA update

--- device.mk
+++ device.mk
@@ -170,5 +170,16 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
     ro.product.manufacturer \
     ro.product.model
 
+# OTAUpdates
+PRODUCT_PACKAGES += \
+    OTAUpdates
+
+# OTA dependencies
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.ota.romname=cm-13.0-seed \
+	ro.ota.version=$(shell date +%Y%m%d) \
+	ro.ota.manifest=https://www.dropbox.com/s/9ie5j4p3k6z8qz9/ota.xml?raw=1
+
+
 # Inherit the rest from msm8916-common
 $(call inherit-product, device/cyanogen/msm8916-common/msm8916.mk)
