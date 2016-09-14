e92db84101a89b20ceca7e116355417d01eab821
Arvin Quilao <arquilao@gmail.com>
2016-09-14 03:37:08 +0000

    Use seed kernel source
    Change-Id: I13096af77ad3dc354f9561e351935ff2e9561909

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,4 +1,9 @@
 # Kernel
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
-TARGET_KERNEL_CONFIG := cyanogenmod_seed_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/seed
+TARGET_KERNEL_CONFIG := msm8916-perf_defconfig
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+TARGET_KERNEL_APPEND_DTB := true
