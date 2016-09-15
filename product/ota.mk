00a3e1d85a00e242b8707404937efab47ab55fa0
Arvin Quilao <arquilao@gmail.com>
2016-09-15 01:09:55 +0000

    Remove OTAUPdates package
    Change-Id: I6bf7e50410b2bd3f1c34c1961d158cdde55776d9

--- product/ota.mk
+++
@@ -1,9 +0,0 @@
-# OTAUpdates
-PRODUCT_PACKAGES += \
-    OTAUpdates
-
-# OTA dependencies
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.ota.romname=cm-13.0-seed \
-	ro.ota.version=$(shell date +%Y%m%d) \
-	ro.ota.manifest=https://www.dropbox.com/s/9ie5j4p3k6z8qz9/ota.xml?raw=1
