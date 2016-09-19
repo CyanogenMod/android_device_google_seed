142a326e38aa9a9499f36bbb11d83536cdfa7d1a
Arvin Quilao <arquilao@gmail.com>
2016-09-19 02:58:09 +0000

    Use new defconfig file
    Change-Id: Iaffd8c32edbefa5d3061cc18e5bb81d4f14c04e6

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,7 +2,7 @@
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_SOURCE := kernel/google/seed
-TARGET_KERNEL_CONFIG := msm8916-perf_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_seed_defconfig
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
